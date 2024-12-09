fa7cba1d284a7b44211d3bd6d739d97999eaf556
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-12-09 18:32:30 +0100

    Using a Reactive Form for updating an Article

--- src/app/newpost/newpost.component.ts
+++ src/app/newpost/newpost.component.ts
@@ -1,15 +1,16 @@
-import { Component, effect, inject, SecurityContext } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, effect, inject, OnInit, SecurityContext } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import { DomSanitizer } from '@angular/platform-browser';
-
 import { QuillModule } from 'ngx-quill';
 import { DataService } from '../shared/data.service';
-import { ArticleDTO } from '../objects/dataObjects';
+import { ArticleDTO, ICategory } from '../objects/dataObjects';
 import { quilEditorlModules } from '../objects/quillObjects';
 import { Location } from '@angular/common';
 import { ContentService } from '../shared/content.service';
@@ -25,15 +26,15 @@ const ComponentName = 'NewpostComponent';
     MatDividerModule,
     MatIconModule,
     MatButtonModule,
-    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
     QuillModule,
   ],
   templateUrl: './newpost.component.html',
   styleUrl: './newpost.component.scss',
 })
-export class NewpostComponent {
-  
-  
+export class NewpostComponent implements OnInit{
   
   
   editorContent: string = '';
@@ -44,79 +45,108 @@ export class NewpostComponent {
 
   public article: ArticleDTO = new ArticleDTO();
 
-  private contentService = inject(ContentService);
+  postForm!: FormGroup;
+  categoryOptions: ICategory[] = [];
+
   // private router = inject(Router);
+  private sanitizer = inject( DomSanitizer);
+  private contentService = inject(ContentService);
+  private snackBar = inject( MatSnackBar);
+  private dataService = inject( DataService);
+  private location = inject(Location);
+  private fb= inject(FormBuilder);
+  private isNew: boolean = true;
   
   constructor(
-    private snackBar: MatSnackBar,
-    private sanitizer: DomSanitizer,
-    private dataService: DataService,
-    private location: Location
+
   ) {
     effect(() => {
-      this.article = this.contentService.$article();
-      if (this.contentService.$newPost() === 2) this.editorContent = this.article.articleContent;
+      // The ngOnInit runs before the constructor.
+      // Moreover, we do Not need to constantly monitor the changes of signals,
+      // we need just once to capture the article and fill0in the form fields.
+      // So, the job can be done *solo* in the ngOnInit method.
+
+      // this.article = this.contentService.$article();
+      // this.categoryOptions = this.contentService.$categories();
+      // if (this.contentService.$newPost() == 2) this.isNew = false;   // 2 means existing post
+      // console.log( '>===>> ' + ComponentName + ' - Constructor - ' + 'New Post? ' + this.contentService.$newPost());
+      // this.editorContent = this.isNew ? '' : this.article.articleContent;
+      // console.log( '>===>> ' + ComponentName + ' - Constructor - ' + 'Is a New Post? ' + this.isNew);
+      // this.initForm();
     });
-
   }
 
-  // The editorInit uses the clipboard matcher functionality to
-  // remove background colors when pasting content into the
-  // ngx-quill editor.
 
-  // Handle pasted content
-  editorInit(quill: any) {
-    quill.clipboard.addMatcher(
-      Node.ELEMENT_NODE,
-      function (node: any, delta: any) {
-        delta.forEach((op: any) => {
-          if (op.attributes) {
-            // Remove background color
-            delete op.attributes.background;
-            // Remove text color 
-            delete op.attributes.color;
-          }
-          // Handle text content - replace &nbsp; with regular space
-          if (op.insert && typeof op.insert === 'string') {
-            op.insert = op.insert.replace(/&nbsp;/g, ' ');
-          }
-        });
-        return delta;
-      }
-    );
+  ngOnInit() {
+    this.article = this.contentService.$article();
+    this.categoryOptions = this.contentService.$categories();
+    if (this.contentService.$newPost() == 2) this.isNew = false;
+    this.editorContent = this.isNew ? '' : this.article.articleContent;
+    // console.log( '>===>> ' + ComponentName + ' - OnInit - ' + 'New Post? ' + this.contentService.$newPost());
+    // console.log( '>===>> ' + ComponentName + ' - OnInit -' + 'Is a New Post? ' + this.isNew);
+    this.initForm();
   }
+
+
+
+
+
+  // Initialize the form
+  private initForm() {
+    this.postForm = this.fb.group({
+      title: [this.isNew ? '' : this.article.articleTitle, [Validators.required, Validators.minLength(5)]],
+      subtitle: [this.isNew ? '' : this.article.articleSubTitle, Validators.required],
+      description: [this.isNew ? '' : this.article.articleDescription, Validators.required],
+      slug: [this.isNew ? '' : this.article.articleSlug, Validators.required],
+      category: [this.isNew ? '' : this.article.categoryId, Validators.required],
+      content: [this.isNew ? '' : this.editorContent, Validators.required]
+    });
+  }
+
+
+  // Getter methods for form controls
+  get title() { return this.postForm.get('title'); }
+  get subtitle() { return this.postForm.get('subtitle'); }
+  get description() { return this.postForm.get('description'); }
+  get slug() { return this.postForm.get('slug'); }
+  get category() { return this.postForm.get('category'); }
+  get content() { return this.postForm.get('content'); }
+
+
 
   cancel() {
     this.location.back();
   }
 
-
-  saveContent() {
-    if (this.contentService.$newPost() === 2) {
-
-      this.article.articleContent = this.getQuillEditorContent();
-      // console.log('>===>> ' + ComponentName + ' - ' +'Updating existing article ' + this.article.articleClientUUID);
-      this.updateArticle(this.article);
-
-    } else if (this.contentService.$newPost() === 1) {
-      const newArticle: ArticleDTO = new ArticleDTO();
-      newArticle.userId = 1;
-      newArticle.categoryId = 1;
-      newArticle.articleTitle = 'New Article';
-      newArticle.articleSubTitle = 'New Article Sub-Title ';
-      newArticle.articleDescription = 'New Article Description';
-      newArticle.articleSlug = 'new-article'; 
-      newArticle.articleContent = this.getQuillEditorContent();
-      this.addArticle(newArticle);
+  onSubmit() {
+    if (this.postForm.valid) {
+      const formValues = this.postForm.value;
+      this.article.articleTitle = formValues.title;
+      this.article.articleSubTitle = formValues.subtitle;
+      this.article.articleDescription = formValues.description;
+      this.article.articleSlug = formValues.slug;
+      this.article.categoryId = formValues.category; 
+      this.article.articleContent = this.cleanupContent(formValues.content);
+      if (this.isNew) {
+        this.addArticle(this.article);
+      } else  {
+        this.updateArticle(this.article);
+      } 
+    } else {
+      this.markFormGroupTouched(this.postForm);
     }
   }
 
-  private getQuillEditorContent(): string {
-    return this.editorContent.trim().length === 0
-      ? ''
-      : this.cleanupContent(this.editorContent);
-  }
+  // Helper method to mark all controls as touched
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
 
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
 
 
   private addArticle(newArticle: ArticleDTO) {
@@ -145,6 +175,35 @@ export class NewpostComponent {
         this.showSnackBar(['Error! ' + err]);
       },
     });
+  }
+
+
+
+
+  // The editorInit uses the clipboard matcher functionality to
+  // remove background colors when pasting content into the
+  // ngx-quill editor.
+
+  // Handle pasted content
+  editorInit(quill: any) {
+    quill.clipboard.addMatcher(
+      Node.ELEMENT_NODE,
+      function (node: any, delta: any) {
+        delta.forEach((op: any) => {
+          if (op.attributes) {
+            // Remove background color
+            delete op.attributes.background;
+            // Remove text color 
+            delete op.attributes.color;
+          }
+          // Handle text content - replace &nbsp; with regular space
+          if (op.insert && typeof op.insert === 'string') {
+            op.insert = op.insert.replace(/&nbsp;/g, ' ');
+          }
+        });
+        return delta;
+      }
+    );
   }
 
 
